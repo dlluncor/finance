8161233ff18edc5c6cb95d88945f954956b495b2
dlluncor <david.lluncor@gmail.com>
2013-03-16 15:15:08 -0700

    hello

--- financier/src/proj/easydo/index.py
+++ financier/src/proj/easydo/index.py
@@ -4,12 +4,14 @@ End points for easydo applications.
 
 import pdb
 # Self defined modules.
+import easydoer
 
 # Generic modules.
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 
 import logging
+import math
 
 import os
 from django.utils import simplejson
@@ -21,7 +23,63 @@ class FlightContacter(object):
   
   def __init__(self, xml_source = Constants.DEFAULT_SOURCE):
     self.xml_source = xml_source
-    pass
+
+  def HasCheapestFlightInfo(self):
+    samples = self.GetPreviousFlightPrices()
+    current_prices = self.GetCurrentPrices()
+    stats = Stats(samples)
+    for cur_price in current_prices:
+      info = stats.isCheapFlightInfo(cur_price)
+      if info['is_cheap_flight']:
+        return info
+    return None
+
+  def GetPreviousFlightPrices(self):
+    prices = [2000.00,2000.00,2000.00,2000.00,2850.00,1850.00,1850.00,2850.00,2850.00,
+    2850.00,2850.00,2850.00,2850.00,2850.00,4170.00,6380.00,6090.00,8755.00,1500.00,
+    1500.00,1500.00,1350.00,1350.00,2350.00,2350.00,2350.00,2350.00,2350.00,2350.00,
+    2350.00,4570.00,4570.00,4570.00,6890.00,8755.00]
+    return [price / 10.0 for price in prices]
+
+  def GetCurrentPrices(self):
+    return [200, 500]
+
+
+class Stats(object):
+
+  def __init__(self, samples):
+    self.samples = samples
+    self.mean = self.CalculateMean()
+    self.sampling_stdev_sample_mean = self.CalculateStdevMean()
+
+  def CalculateStdevMean(self):
+    """Computes sampling distribution of sample mean."""
+    n = len(self.samples) * 1.0
+    mean = sum(self.samples) / n
+    sum_sq_dist = 0
+    for sample in self.samples:
+      sum_sq_dist += math.pow(sample - mean, 2)
+
+    s = math.sqrt(sum_sq_dist / (n - 1))
+    sampling_stdev_mean = s / math.sqrt(n)
+    return sampling_stdev_mean
+
+  def CalculateMean(self):
+    return sum(self.samples) / (len(self.samples) * 1.0)
+
+  def isCheapFlightInfo(self, flight_sample):
+    """Returns whether this flight price is 95 percent less than all samples taken."""
+    self.z_score_thresh = 1.96
+    my_zscore = (flight_sample - self.mean) / self.sampling_stdev_sample_mean
+
+    d = {}
+    d['is_cheap_flight'] = abs(my_zscore) > self.z_score_thresh
+    d['discount'] = (self.mean - flight_sample)
+    d['flight_cost'] = flight_sample
+    d['zscore'] = my_zscore
+    d['mean'] = self.mean
+    d['sample_stdev'] = self.sampling_stdev_sample_mean
+    return d
 
 ## JSON endpoints.
 class TriggerEndPoint(webapp.RequestHandler):
@@ -29,9 +87,18 @@ class TriggerEndPoint(webapp.RequestHandler):
    d = {}
    for key in self.request.GET:
      d[key] = str(self.request.GET[key])
+
+   # Start contacting flight sources to find cheapest flight.
+   flighter = FlightContacter()
+   flight_info = flighter.HasCheapestFlightInfo()
+   if flight_info:
+    d['flight_info'] = flight_info
+
+   # If we have found a cheap flight, send a trigger to EasyDo.
+   easydo = easydoer.EasyDoer()
+   easydo.Run()
+
    #pdb.set_trace()
-   results = []
-   d['results'] = results
    json = simplejson.dumps(d)
    self.response.headers.add_header('content-type', 'application/json', charset='utf-8') 
    self.response.out.write(json)
